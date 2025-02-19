8dc7285ad42e004d84f2683e34b3ae762521cdc8
Matěj Jiránek <145572111+matejak47@users.noreply.github.com>
2025-02-19 22:03:41 +0100

    Testing app

--- src/main/java/com/example/burza/BurzaApplication.java
+++ src/main/java/com/example/burza/BurzaApplication.java
@@ -2,6 +2,7 @@ package com.example.burza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class BurzaApplication {
@@ -9,5 +10,5 @@ public class BurzaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BurzaApplication.class, args);
 	}
-
+		int a;
 }
