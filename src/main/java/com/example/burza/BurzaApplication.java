93ed8ee6b6d181e7b6578349e0f5590478e8d918
Matěj Jiránek <145572111+matejak47@users.noreply.github.com>
2025-02-19 21:47:41 +0100

    Start project

---
+++ src/main/java/com/example/burza/BurzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.burza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/burza/BurzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.burza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
