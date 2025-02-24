54a9ba6360c3169a93cbcfc2b5490d5c8b4e0583
rajusus <esocugalik@protonmail.com>
2025-02-24 20:19:46 +0100

    Tests of models

--- src/main/java/com/example/burza/model/Symbol.java
+++ src/main/java/com/example/burza/model/Symbol.java
@@ -1,11 +1,15 @@
 package com.example.burza.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Data model representing a stock symbol.
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Symbol {
     private String symbol;
     private String name;
